b1628850418db630f4e6211de584923e0f469dd7
Ugosylva <nugosylva@gmail.com>
2022-09-19 12:55:15 -0700

    Task 1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H                                                            
-#define MAIN_H                                                            
-                                                                          
-/**                                                                       
-* void prototypes()                                                       
-* int prototypes()                                                        
-*/
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -17,5 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif /*MAIN_H*/
